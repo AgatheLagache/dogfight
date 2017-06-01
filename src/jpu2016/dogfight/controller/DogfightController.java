c930f6b60e171003f52d9c6f251989469883dc66
Agathe <Agathe@10.133.129.7>
2017-06-01 10:53:17 +0200

    Phase 3

---
+++ src/jpu2016/dogfight/controller/DogfightController.java
@@ -0,0 +1,35 @@
+package jpu2016.dogfight.controller;
+
+import jpu2016.digfight.model.IDogfightModel;
+import jpu2016.dogfight.view.IViewSystem;
+
+public class DogfightController implements IOrderPerformer{
+	private static int TIME_SLEEP = 30;
+
+	public DogfightController(IDogfightModel dogfightModel){
+
+	}
+
+	private void gameLoop(){
+
+	}
+
+	private void lauchMissile(int player){
+
+	}
+
+	@Override
+	public void orderPerform(UserOrder userOrder) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void play(){
+
+	}
+
+	public void setViewSystem(IViewSystem viewSystem){
+
+	}
+
+}
