56ee7dc7ee5a570d5f160cefff6028fe88bc6d86
Agathe <Agathe@10.133.129.7>
2017-06-02 08:42:29 +0200

    Phase 3 (3)

--- src/dogfight.java
+++ src/dogfight.java
@@ -1,8 +1,13 @@
+import jpu2016.dogfight.controller.DogfightController;
 
 public class dogfight {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		final DogfightModel dogfightModel = new DogfightModel();
+		final DogfightController dogfightController = new DogfightController(dogfightModel);
+		final DogfightView dogfightView = new DogfightView(dogfightController, dogfightModel, dogfightModel);
+		dogfightController.setViewSystem(dogfightView); dogfightController.play();
 
 	}
 
